e62acb16bdfa4c2c725bc177a629776838e83d96
ikingcodex <abangelsha@gmail.com>
2017-02-09 14:37:00 +0100

    completed form validation

--- js/validate.js
+++ js/validate.js
@@ -1,23 +1,28 @@
 function signup(){
   var username = document.signform.username.value;
+  var ck_username = /^[A-Za-z0-9_]{6,20}$/;
   var email = document.signform.email.value;
   var pnumber = document.signform.pnumber.value;
   var accnumber = document.signform.accnumber.value;
   var accname = document.signform.accname.value;
-  // var bank = document.signform.bank.value;
+  var bank = document.signform.bank.value;
   var password = document.signform.password.value;
   var rpassword = document.signform.rpassword.value;
 
-  if ((username == null || username == "") && (email == null || email == "") && (pnumber == null || pnumber == "")){
+  if ((username == null || username == "") || (email == null || email == "") || (pnumber == null || pnumber == "")){
     alert("please fill in all fields, as all fields are required.");
     return false;
   }
-  if ((accnumber == null || accnumber == "") && (accname == null || accname == "") && (password == null || password == "")){
+  if ((accnumber == null || accnumber == "") || (accname == null || accname == "") || (password == null || password == "")){
     alert("please fill in all fields, as all fields are required.");
     return false;
   }
-  if (rpassword == null || rpassword == ""){
+  if ((rpassword == null || rpassword == "") || (bank == null || bank == "")){
     alert("please fill in all fields, as all fields are required.");
+    return false;
+  }
+  if (!ck_username.test(username)) {
+    alert("username can contain alphabets and numbers no special characters except underscore('_') min 6 and max 20 characters. ")
     return false;
   }
   var atposition = email.indexOf("@");
@@ -42,13 +47,25 @@ function signup(){
     alert("password must be same!");
     return false;
   }
+ //  if (bank == null || bank == ""){
+ //   alert("nothing was selected");
+ //   return false;
+ // }
+ // else {
+ //   alert(bank + " was selected");
+ // }
 }
 
 function login(){
   var username = document.logform.username.value;
+  var ck_username = /^[A-Za-z0-9_]{6,20}$/;
   var password = document.logform.password.value;
   if (username == null || username == ""){
     alert("Username field cannot be empty");
+    return false;
+  }
+  if (!ck_username.test(username)) {
+    alert("please enter a valid username, username must be atleast 6 characters long")
     return false;
   }
   if (password == null || password == ""){
